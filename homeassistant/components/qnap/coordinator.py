11396b52ac2cf9b08a7f1a3ffaaf28295be01ecd
disforw <disforw@users.noreply.github.com>
2026-04-06 02:43:20 +0000

    qnap: fix mypy type errors in coordinator

--- homeassistant/components/qnap/coordinator.py
+++ homeassistant/components/qnap/coordinator.py
@@ -45,7 +45,7 @@ def suppress_insecure_request_warning():
         yield
 
 
-class QnapCoordinator(DataUpdateCoordinator[dict[str, dict[str, Any]]]):
+class QnapCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     """Custom coordinator for the qnap integration."""
 
     config_entry: QnapConfigEntry
@@ -72,7 +72,7 @@ class QnapCoordinator(DataUpdateCoordinator[dict[str, dict[str, Any]]]):
             timeout=config_entry.data.get(CONF_TIMEOUT),
         )
 
-    def _sync_update(self) -> dict[str, dict[str, Any]]:
+    def _sync_update(self) -> dict[str, Any]:
         """Get the latest data from the Qnap API."""
         with (
             suppress_insecure_request_warning()
@@ -88,6 +88,6 @@ class QnapCoordinator(DataUpdateCoordinator[dict[str, dict[str, Any]]]):
                 "firmware_update": self._api.get_firmware_update(),
             }
 
-    async def _async_update_data(self) -> dict[str, dict[str, Any]]:
+    async def _async_update_data(self) -> dict[str, Any]:
         """Get the latest data from the Qnap API."""
         return await self.hass.async_add_executor_job(self._sync_update)
